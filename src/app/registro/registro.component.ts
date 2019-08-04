d21aa0a8420c50f870acae2389d5444d5cdebe41
francisco valerio <franciscojvg0607@gmail.com>
2019-08-04 11:56:04 -0500

    cambios en registro y login

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -5,6 +5,9 @@ import { UsuarioService } from '../services/usuario/usuario.service';
 import { Usuario } from '../models/usuario.model';
 import { Router } from '@angular/router';
 
+
+declare const gapi: any;
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -13,11 +16,34 @@ import { Router } from '@angular/router';
 export class RegistroComponent implements OnInit {
 
   forma: FormGroup;
+  auth2:any;
 
   constructor(public usuarioService:UsuarioService,
               public router: Router) { }
 
+
+              sonIguales(campo1:string,campo2:string){
+
+                
+
+                return(group: FormGroup)=>{
+                  let pass1= group.controls[campo1].value;
+                  let pass2= group.controls[campo2].value;
+
+                  if (pass1 === pass2) {
+                    return null;
+                    
+                  }
+
+                  return{
+                    sonIguales:true
+                  };
+                }
+
+              }
+
   ngOnInit() {
+    this.googleInit();
 
     this.forma = new FormGroup({
 
@@ -26,10 +52,11 @@ export class RegistroComponent implements OnInit {
       telefono: new FormControl(null, Validators.required),
       email: new FormControl(null,[ Validators.required, Validators.email]),
       password: new FormControl(null, Validators.required),
+      password2: new FormControl(null, Validators.required),
       condiciones: new FormControl(false),
 
 
-    });
+    }, {validators: this.sonIguales('password','password2') });
 
   }
 
@@ -46,6 +73,7 @@ export class RegistroComponent implements OnInit {
       )
 
       
+
       return;
       
     }
@@ -61,6 +89,31 @@ export class RegistroComponent implements OnInit {
       .subscribe(resp =>{
         this.router.navigate(['/login']);
       });
+  }
+
+  googleInit(){
+    gapi.load('auth2',()=>{
+      this.auth2 = gapi.auth2.init({
+        client_id: '732239912806-oua2pe3kobchn9g8dr5daedvlf8q74i2.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('btnGoogle'));
+    });
+  }
+
+
+  attachSignin(element){
+this.auth2.attachClickHandler(element, {}, (googleUser) =>{
+
+  //let profile = googleUser.getBasicProfile();
+  let token = googleUser.getAuthResponse().id_token;
+
+  this.usuarioService.loginGoogle(token)
+  .subscribe(resp=>{
+    window.location.href = '#/dashboard'
+  })
+});
   }
 
 }
